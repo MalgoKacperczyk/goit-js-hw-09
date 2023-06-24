e9fa7bed860bc2abdf92ab59ee9cb4dedd867abc
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-06-24 22:08:11 +0200

    add changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,18 +2,18 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 function changeColor() {
-    const startButton = document.querySelector('button[data-start]');
-    const stopButton = document.querySelector('button[data-stop]');
-    const body = document.querySelector('body');
-    const intervalId;
+  const startButton = document.querySelector('button[data-start]');
+  const stopButton = document.querySelector('button[data-stop]');
+  const body = document.querySelector('body');
+  let intervalId;
 
-    startButton.disabled = true;
-    intervalId = setInterval(function () {
-        body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
+  startButton.disabled = true;
+  intervalId = setInterval(function () {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 
-    closeButton.addEventListener('click', function () {
-        clearInterval(intervalId);
-        startButton.disabled = false;
-    });
+  stopButton.addEventListener('click', function () {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+  });
 }
